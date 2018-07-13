22e927fa820f65413ca134e4e5da0689158d67ea
Amee Covarrubias <ameecova@yahoo.com>
2018-07-13 06:58:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,4 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 
     "Fizz"
+  elsif int % 5 == 0 
+    return "Buzz"
+  elsif int % 3 == 0 && int & 5 == 0 
+    return "FizzBuzz"
+  else 
+    return nil 
+  
 end
